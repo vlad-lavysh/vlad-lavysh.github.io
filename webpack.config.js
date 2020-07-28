886fd576d7eb7ace41ba3ed5ed6c2a8949880cba
Vlad <57154809+Vlad043@users.noreply.github.com>
2020-07-28 12:45:19 +0200

    Finally set up damn webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HTMLWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
@@ -14,12 +15,22 @@ module.exports = {
       template: "./index.pug",
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+    }),
   ],
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {},
+          },
+          "css-loader",
+          "sass-loader",
+        ],
       },
       {
         test: /\.pug$/,
