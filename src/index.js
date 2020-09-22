b46a605d17d72e3a45162479c21d9a92e7ee8dad
Vlad <57154809+Vlad043@users.noreply.github.com>
2020-09-22 21:07:03 +0200

    Refactoring (images)

--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@ import "./main.scss";
 
 import "./includes/headers-footers/header/header";
 
-import "./images/avatar_1.png";
-import "./images/avatar_2.png";
+import "./includes/ui-kits/review/images/avatar_1.png";
+import "./includes/ui-kits/review/images/avatar_2.png";
 
-import "./images/enter_background.png";
-import "./images/search_background.png";
+import "./pages/enter/images/enter_background.png";
+import "./pages/search/images/search_background.png";
 
-import "./images/room_1.png";
-import "./images/room_2.png";
-import "./images/room_3.png";
-import "./images/room_4.png";
-import "./images/room_5.png";
-import "./images/room_6.png";
-import "./images/room_7.png";
-import "./images/room_8.png";
-import "./images/room_9.png";
-import "./images/room_10.png";
-import "./images/room_11.png";
-import "./images/room_12.png";
+import "./pages/rooms/images/room_1.png";
+import "./pages/rooms/images/room_2.png";
+import "./pages/rooms/images/room_3.png";
+import "./pages/rooms/images/room_4.png";
+import "./pages/rooms/images/room_5.png";
+import "./pages/rooms/images/room_6.png";
+import "./pages/rooms/images/room_7.png";
+import "./pages/rooms/images/room_8.png";
+import "./pages/rooms/images/room_9.png";
+import "./pages/rooms/images/room_10.png";
+import "./pages/rooms/images/room_11.png";
+import "./pages/rooms/images/room_12.png";
 
-import "./images/room_bg_1.png";
-import "./images/room_bg_2.png";
-import "./images/room_bg_3.png";
+import "./pages/room-details/images/room_bg_1.png";
+import "./pages/room-details/images/room_bg_2.png";
+import "./pages/room-details/images/room_bg_3.png";
